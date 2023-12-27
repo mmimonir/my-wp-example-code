203f0d8825a03366d7f6c9d55aa5c2940ae6882f
Md Monirul Islam <mmimonir.ctg@gmail.com>
2023-12-27 18:26:42 +0600

    theme file init

--- themes/twentytwentyone/index.php
+++ themes/twentytwentyone/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -16,30 +17,29 @@
 
 get_header(); ?>
 
-<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
+<?php if (is_home() && !is_front_page() && !empty(single_post_title('', false))) : ?>
 	<header class="page-header alignwide">
 		<h1 class="page-title"><?php single_post_title(); ?></h1>
 	</header><!-- .page-header -->
 <?php endif; ?>
-
+<h1>My Test Code</h1>
 <?php
-if ( have_posts() ) {
+if (have_posts()) {
 
 	// Load posts loop.
-	while ( have_posts() ) {
+	while (have_posts()) {
 		the_post();
 
-		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
+
+		get_template_part('template-parts/content/content', get_theme_mod('display_excerpt_or_full_post', 'excerpt'));
 	}
 
 	// Previous/next page navigation.
 	twenty_twenty_one_the_posts_navigation();
-
 } else {
 
 	// If no content, include the "No posts found" template.
-	get_template_part( 'template-parts/content/content-none' );
-
+	get_template_part('template-parts/content/content-none');
 }
 
 get_footer();
